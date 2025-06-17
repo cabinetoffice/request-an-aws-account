59ae6f8abaee053ddbbcca7497497c9b6aec213d
Harley Harris <harley.harris@cabinetoffice.gov.uk>
2025-06-17 13:00:41 +0100

    add aft implementation for github and terraform service code and tests

--- app/models/account_details_form.rb
+++ app/models/account_details_form.rb
@@ -1,12 +1,18 @@
 class AccountDetailsForm
   include ActiveModel::Model
 
+  # Max account name length allowed in AWS is 64 characters
+  AWS_ROOT_ACCOUNTS_EMAIL_BASE_LENGTH = 30
+  MAX_EMAIL_LENGTH = 64
+  MAX_ACCOUNT_NAME_LENGTH = MAX_EMAIL_LENGTH - AWS_ROOT_ACCOUNTS_EMAIL_BASE_LENGTH
+
   attr_reader :account_name, :account_description
 
   validates_format_of :account_name, with: /\A([a-z]+-)*[a-z]+\z/, message: 'should be lower-case-separated-by-dashes'
   validates_format_of :account_name, with: /\A([a-z]+-){0,4}[a-z]+\z/, message: 'should not have more-than-five-groups-separated-by-dashes'
 
-  validates :account_name, :account_description, presence: true, length: { maximum: 256 }
+  validates :account_name, length: { maximum: MAX_ACCOUNT_NAME_LENGTH, message: "should not be longer than #{MAX_ACCOUNT_NAME_LENGTH} characters" }
+  validates :account_description, presence: true, length: { maximum: 256 }
 
   validates_format_of :account_name,
                       :account_description,
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,86 +1,96 @@
 require 'octokit'
 
 class GithubService
+    MAIN_BRANCH = 'main'
+    
   def initialize
     if ENV.has_key? 'GITHUB_PERSONAL_ACCESS_TOKEN'
       @client = Octokit::Client.new(access_token: ENV['GITHUB_PERSONAL_ACCESS_TOKEN'])
     end
+    @github_repo = ENV.fetch('GITHUB_REPO') { raise "Missing required environment variable: GITHUB_REPO" }
   end
 
-  def create_new_account_pull_request(account_name, account_description, email, admin_users, tags, host)
+  def create_new_account_pull_request(account_name, account_description, email, admin_users, account_terraform_file, host)
     unless @client
       Errors::log_error 'No GITHUB_PERSONAL_ACCESS_TOKEN set. Skipping pull request.'
       return nil
     end
 
-    new_branch_name = 'new-aws-account-' + account_name
-    github_repo = 'alphagov/aws-billing-account'
-    master = @client.commit(github_repo, 'master')
-    create_branch github_repo, new_branch_name, master.sha
+    new_branch_name = "new-aws-account-#{account_name}"
+    latest_commit_on_main = @client.commit(@github_repo, MAIN_BRANCH)
+    new_file_filepath = "terraform/#{account_name}.tf"
+    pull_request_title = "Add new AWS account for: #{account_name}"
+    account_description_quote = format_account_description(account_description)
+    commit_message = create_commit_message(account_name, account_description_quote, email)
+    pull_request_body = create_pull_request_body(account_name, account_description_quote, email, admin_users)
 
-    for accounts_path in ['terraform/accounts.tf.json', 'terraform/accounts.tf'] do
-      begin
-        accounts_contents = @client.contents github_repo, path: accounts_path
-      rescue Octokit::NotFound
-        # allow a final failure to be detected
-        accounts_path = nil
-      else
-        break
-      end
-    end
-    unless accounts_path
-      Errors::log_error "Couldn't locate accounts terraform file."
-      return nil
-    end
+    # check if account terraform file already exists on main branch
+    check_if_file_already_exists(@github_repo, MAIN_BRANCH, new_file_filepath)
 
-    name = email.split('@').first.split('.').map { |name| name.capitalize }.join(' ')
-    terraform_accounts_service = TerraformAccountsService.new(Base64.decode64(accounts_contents.content))
-    new_account_terraform = terraform_accounts_service.add_account(account_name, tags)
-    account_description_quote = account_description.split(/\r?\n/).map {|desc| "> #{desc}"}.join("\n")
-    @client.update_contents(
-      github_repo,
-      accounts_path,
-      # TODO add cost center an org in here.
-      "Add new AWS account for: #{account_name} 
+    # create branch for new aws account 
+    create_new_aws_account_branch(@github_repo, new_branch_name, latest_commit_on_main.sha)
 
-Description:
-#{account_description_quote}
+    # create the aws account terraform file on new branch
+    @client.create_contents(@github_repo, new_file_filepath, commit_message, account_terraform_file, branch: new_branch_name)
 
-Co-authored-by: #{name} <#{email}>",
-      accounts_contents.sha,
-      new_account_terraform,
-      branch: new_branch_name
-    )
-    @client.create_pull_request(
-      github_repo,
-      'master',
-      new_branch_name,
-      # TODO add org and const centre in this string
-      "Add new AWS account for: #{account_name}",
-      "Account requested using #{host} by #{email}
-
-Description:
-#{account_description_quote}
-
-Once the account is created, the following users should be granted access to the admin role:
-
-```
-#{admin_users}
-```"
-    ).html_url
+    # raise the PR
+    return @client.create_pull_request(@github_repo, MAIN_BRANCH, new_branch_name, pull_request_title, pull_request_body).html_url
   end
 
-  private
+  def format_account_description(account_description)
+    return account_description.split(/\r?\n/).map {|desc| "> #{desc}"}.join("\n")
+  end
 
-  def create_branch(repo, branch_name, sha)
+  def create_commit_message(account_name, account_description_quote, email)
+    name = email.split('@').first.split('.').map { |name| name.capitalize }.join(' ')
+
+    ###
+    commit_message = <<~TEXT
+    Add new AWS account for: #{account_name} 
+
+    Description:
+    #{account_description_quote}
+
+    Co-authored-by: #{name} <#{email}>"
+    TEXT
+    ###
+
+    return commit_message
+  end
+
+  def create_pull_request_body(host, account_description_quote, email, admin_users)
+    ###
+    pull_request_body = <<~TEXT 
+    Account requested using #{host} by #{email}
+
+    Description:
+    #{account_description_quote}
+
+    Once the account is created, the following users should be granted access to the admin role:
+
+    ```
+    #{admin_users}
+    ```
+    TEXT
+    ###
+
+    return pull_request_body
+  end
+
+  def check_if_file_already_exists(repo, branch_name, filepath)
+    # raise error if the new file being created already exists
+    if @client.contents(repo, path: filepath, ref: branch_name)
+      raise Errors::AccountTerraformFileAlreadyExistsError, filepath
+    end
+  rescue Octokit::NotFound
+    Rails.logger.info("Creating account Terraform file as it does not exist")
+  end
+
+  def create_new_aws_account_branch(repo, branch_name, sha)
     begin
       @client.create_ref repo, 'heads/' + branch_name, sha
     rescue Octokit::UnprocessableEntity => e
       Errors::log_error "Failed to create branch #{branch_name}. Perhaps there's already a branch with that name?", e
     end
-  end
-
-  def multiple_users?(email_list)
-    email_list.split('@').size > 2
   end
 end
